fa7c1bdecd8aef691c39b7f1ae119a83bdf99b2d
Arafath987 <94624401+Arafath987@users.noreply.github.com>
2024-04-03 12:57:12 +0530

    rzorpay installation not working

--- web/app/ordering/page.jsx
+++ web/app/ordering/page.jsx
@@ -2,8 +2,43 @@
 
 import {  Flex, Button, Link, Text } from "@chakra-ui/react";
 import { MdOutlineProductionQuantityLimits } from "react-icons/md";
+// import {Razorpay} from "razorpay"
+var razorpay = require('razorpay')
 
 const page = () => {
+  
+  function razor(){
+    var options = {
+      "key": "rzp_test_x9fBn06d4xJl9V", // Enter the Key ID generated from the Dashboard
+      "amount": "1000", // Amount is in currency subunits. Default currency is INR. Hence, 50000 refers to 50000 paise
+      "currency": "INR",
+      "name": "Acme Corp", //your business name
+      "description": "Test Transaction",
+      "image": "https://example.com/your_logo",
+      "order_id": "order_9A33XWu170gUtm", //This is a sample Order ID. Pass the `id` obtained in the response of Step 1
+      "handler": function (response){
+          // alert(response.razorpay_payment_id);
+          // alert(response.razorpay_order_id);
+          // alert(response.razorpay_signature)
+
+          verify(order,response)
+      },
+      "prefill": { //We recommend using the prefill parameter to auto-fill customer's contact information, especially their phone number
+          "name": "Gaurav Kumar", //your customer's name
+          "email": "gaurav.kumar@example.com", 
+          "contact": "9000090000"  //Provide the customer's phone number for better conversion rates 
+      },
+      "notes": {
+          "address": "Razorpay Corporate Office"
+      },
+      "theme": {
+          "color": "#3399cc"
+      }
+  };
+  var rzp1 = new razorpay(options);
+  rzp1.open()
+  }
+        
   return (
     <div>
       <Flex justifyContent="flex-end" marginRight="10px">
@@ -54,7 +89,8 @@ const page = () => {
         >
           Add item
         </Button>
-        <Button
+        <Button 
+         onClick={razor}
           boxShadow="2xl"
           rounded="md"
           borderTopColor="WHITE"
